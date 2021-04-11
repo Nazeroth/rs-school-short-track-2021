6d0db174935e2711be35aafe44a5623852b3e3db
Nazeroth <danieldefoe22@gmail.com>
2021-04-11 13:27:48 +0300

    add: delete digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,10 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arr = n.toString().split('').map(Number);
+  arr.splice(arr.indexOf(Math.min.apply(null, arr)), 1);
+  return +arr.join('');
 }
 
 module.exports = deleteDigit;
